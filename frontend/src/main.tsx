4df4780125520c9124da2f31d448fedae5ba19df
Vitora OS <emmykarl07@gmail.com>
2026-03-27 05:00:59 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-"""\
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -9,4 +9,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
-"""
