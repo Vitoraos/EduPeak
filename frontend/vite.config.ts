0c1501593cc4b53a15ed45da66eab685ab8addd3
Vitora OS <emmykarl07@gmail.com>
2026-03-27 01:52:22 +0100

    Create vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+    }
+  };
+});
